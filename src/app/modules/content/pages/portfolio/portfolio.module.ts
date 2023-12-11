5800e09a907d63e75b8c340fa0ccfac0bdfe8c72
JuliusMatadamas <julius3877@gmail.com>
2023-12-11 10:03:59 -0600

    Se crea el contenido y estilos de la seccion portfolio.

--- src/app/modules/content/pages/portfolio/portfolio.module.ts
+++ src/app/modules/content/pages/portfolio/portfolio.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { PortfolioItemComponent } from './components/portfolio-item/portfolio-item.component';
 
 
 
 @NgModule({
   declarations: [
-    PortfolioComponent
+    PortfolioComponent,
+    PortfolioItemComponent
   ],
   imports: [
     CommonModule
