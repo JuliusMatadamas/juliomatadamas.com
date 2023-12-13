1ea54621aa65bb33f3c6e532ef2a244b75a4a7d9
JuliusMatadamas <julius3877@gmail.com>
2023-12-12 20:40:58 -0600

    Se crea el contenido y estilos de la seccion contact y se empieza a dar estilos responsivos.

--- src/app/modules/content/pages/contact/components/contact/contact.component.ts
+++ src/app/modules/content/pages/contact/components/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faEnvelope, faPhone} from "@fortawesome/free-solid-svg-icons";
+import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
-
+    faPhone = faPhone;
+    faEnvelope = faEnvelope;
+    faLinkedin = faLinkedin;
+    faGithub = faGithub;
 }
--- src/app/modules/content/pages/contact/contact.module.ts
+++ src/app/modules/content/pages/contact/contact.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactComponent } from './components/contact/contact.component';
-
-
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
-  declarations: [
-    ContactComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        ContactComponent
+    ],
+    imports: [
+        CommonModule,
+        FontAwesomeModule
+    ]
 })
+
 export class ContactModule { }
