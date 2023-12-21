1ed764e220add9a606a4700fccf8c03494f308e0
JuliusMatadamas <julius3877@gmail.com>
2023-12-20 18:27:11 -0600

    Se agrega la libreria typed.js

--- src/app/modules/content/pages/home/components/home/home.component.ts
+++ src/app/modules/content/pages/home/components/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+    ngOnInit() {
+        const options = {
+            strings: ['Analista de Datos', 'Desarrollador Fullstack', 'Desarrollador Frontend', 'Desarrollador Backend', 'Programador de aplicaciones', 'Administrador de Bases de Datos'],
+            typeSpeed: 50,
+            loop: true,
+            fadeOut: true,
+        };
 
+        const typed = new Typed('.typing', options);
+    }
 }
